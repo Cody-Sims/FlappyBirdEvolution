fabb98bc74df254577128267c29e1ec29d15200f
Cody-Sims <44505489+Cody-Sims@users.noreply.github.com>
2021-07-16 21:15:13 -0400

    Update App.java

--- App.java
+++ App.java
@@ -4,15 +4,6 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-/**
-  * This is the main class where your FlappyBird Evolution will start.
-  * The main method of this application calls the App constructor. You
-  * will need to fill in the constructor to instantiate your game.
-  *
-  * Class comments here...
-  *
-  */
-
 public class App extends Application {
 
     @Override
@@ -27,11 +18,7 @@ public class App extends Application {
         stage.show();
     }
 
-    /*
-    * Here is the mainline! No need to change this.
-    */
     public static void main(String[] argv) {
-        // launch is a method inherited from Application
         launch(argv);
     }
 }
